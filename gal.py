7d307ce298151f1460f061641a0d0610ccadfcd0
Ayman Bero <55999734+aymencito@users.noreply.github.com>
2020-02-10 00:01:28 +0000

    Update gal.py

---
+++ gal.py
@@ -0,0 +1,5 @@
+## Return a Python dictionary where the key is the id of a spatial unit and the value is a list of the ids of its neighbors.
+
+## Take a gal dictionary from 1 and return a second dictionary that has the histogram for the neighbor cardinalities. The key is the number of neighbors and the value is the list of ids that have key neighbors.
+
+## Take a gal dictionary from 1 and test if there are any asymmetries. An asymmetry occurs when i says j is a neighbor of i, but j does not say i is a neighbor of j.
